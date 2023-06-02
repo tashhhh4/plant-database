9d48a2581d391d3449e24cd2d82f1cca362fe487
Natasha Libera <tash@artoftash.com>
2023-06-02 16:15:55 +0200

    Changed Allowed Hosts to *

--- app/config/settings.py
+++ app/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#)!0wd@d&0ev8bmcpg!^0jbj%5*qyc^8r7)n-7!+eei&*%-cqo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
